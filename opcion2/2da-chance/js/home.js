5cf6acf78fb7b93548e7a991a27a7edfa0e29bcf
Nahuel <gamarraorlandonahuel@gmail.com>
2021-11-24 01:46:02 -0300

    agrego mini inscripcion y datos que se guardan en el localStorage

--- opcion2/2da-chance/js/home.js
+++ opcion2/2da-chance/js/home.js
@@ -1,32 +1,99 @@
-/*$(document).ready(function(){
-    $("a.menu-hamburguesa").click(function(){
-    if($(".menu-hamburguesa").is(":visible")){
-      
-            $(".item2").show();
-        }else{
-            $(".item2").hide();
-        }
-    })
-});
-$(windows).resize(function(){
-    if($(this).width()>768){
-        $("menu-hamburguesa").hide();
-    }
-})*/
 
 
-$(document).ready(function () {
+$(document).ready(function() {
+    $("#belgrano-info").hide();
+    $("#sanjusto-info").hide();
+    $("#ramos-info").hide();
+    $("#caballito-info").hide();
+    
 $("a.menu-hamburguesa").click(function (){
     if($(".item2").is(":visible")){
-        $(".item2").hide();
+       $(".item2").hide();
         console.log("visible")
     } else {
         $(".item2").show();
     }
 })
 
+let opcionOeste= document.getElementsByClassName("oeste");
+let opciones= document.getElementById("provincia");
+let opcionEstablecimiento=document.getElementById("establecimiento");
+opciones.onchange=function(){
+  
+    if(this.value==1){
+        $(".caba").show()
+        $(".oeste").hide()
+    }else{
+        $(".caba").hide()
+        $(".oeste").show()
+    }
+
+    
+};
+
+
+
+opcionEstablecimiento.onchange=function(){
+    $("#belgrano-info").hide();
+    $("#sanjusto-info").hide();
+    $("#ramos-info").hide();
+    $("#caballito-info").hide();
+    console.log(opcionEstablecimiento.text);
+    switch(opcionEstablecimiento.value){
+        case '1':
+            $("#sanjusto-info").hide();
+            $("#ramos-info").hide();
+            $("#caballito-info").hide();
+            $("#belgrano-info").show();
+            break;
+        case "2":
+            $("#belgrano-info").hide();
+            $("#ramos-info").hide();
+            $("#caballito-info").hide();
+            $("#sanjusto-info").show();
+            break;
+        case "3":
+            $("#belgrano-info").hide();
+            $("#sanjusto-info").hide();
+            $("#caballito-info").hide();
+            $("#ramos-info").show();
+            break;
+        case "4":
+            $("#belgrano-info").hide();
+            $("#sanjusto-info").hide();
+            $("#ramos-info").hide();
+         $("#caballito-info").show();
+
+            break;
+    }
+}
+$("#mandarAInscripcion").click( function(){
+    let valor=  $("#establecimiento").val();
+    let texto = $("#establecimiento").find('option:selected').text();
+    localStorage.setItem("establecimiento", texto);
+
+
+    let valor2 = $("#provincia").val();  
+    let texto2 = $("#provincia").find('option:selected').text();
+     localStorage.setItem("provincia", texto2);
+
+       
+ let valor3 =  $("#idioma").val();
+ let texto3 =  $("#idioma").find('option:selected').text(); 
+localStorage.setItem("idioma", texto3);
+console.log(texto3);
+
+   
+let valor4= $("#horario").val(); 
+    
+let texto4 = $("#horario").find('option:selected').text();
+
+localStorage.setItem("horario", texto4);
+console.log(texto4);
+
+
+
 
 });
 
-
-
+})
