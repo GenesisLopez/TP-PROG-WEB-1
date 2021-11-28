b0be0dd8aaaab091a9e9d251a3b8688b82e0ff77
unknown <christiangomez1285@gmail.com>
2021-11-27 23:03:25 -0300

    Agrego parte dubi

---
+++ opcion2/2da-chance/js/validar-ins.js
@@ -0,0 +1,57 @@
+var regexEmail=/^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[0-9a-zA-Z]+$/
+var regexCampoTelefono=/^[0-9]{4}-?[0-9]{4}$/
+var regexDni=/^[0-9]{7,8}$/
+
+// Contacto
+
+function validar(){
+    var error=false;
+    var mensajesError="";
+   
+    if(document.getElementById("nombre").value==''){
+        error=true;
+        mensajesError+="<p>El campo nombre no puede estar vacío</p>";
+    }
+    if(document.getElementById("apellido").value==''){
+        error=true;
+        mensajesError+="<p>El campo apellido no puede estar vacío</p>";
+    }
+    if(document.getElementById("prov").value==''){
+        error=true;
+        mensajesError+="<p>El campo provincia no puede estar vacío</p>";
+    }
+
+    if(!regexEmail.test(document.getElementById("correo").value)){
+        error=true;
+        mensajesError+="<p>Tiene que ser un correo electronico válido</p>";
+    }
+
+    if(document.getElementById("telefono").value==''){
+        error=true;
+        mensajesError+="<p>El campo Telefono no puede estar vacío</p>";
+    }
+    if(document.getElementById("city").value==''){
+        error=true;
+        mensajesError+="<p>El campo Ciudad no puede estar vacío</p>";
+    }
+    if(error){
+        document.getElementById("mensaje").innerHTML=mensajesError;
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function contarCaracteres(obj){
+    var CANTIDAD = 1000;
+    var usados = obj.value.length;
+    var restantes = (CANTIDAD - usados);
+    
+    if(restantes < 0){
+        document.getElementById("numCar").innerHTML = '<span style="color: red;">Cantidad máxima de carcteres 1000</span>';
+    }else if(restantes < 100){
+        document.getElementById("numCar").innerHTML = '<span style="color: orange;"> '+restantes+' caracteres restantes</span>';
+    }else{
+        document.getElementById("numCar").innerHTML = '<span style="color: green;"> '+restantes+' caracteres restantes</span>';
+    }
+}
